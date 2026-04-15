7919570219e1d0db4152934db0c4f754db6a378c
Baris-Saglam <barissaglam099@gmail.com>
2026-04-15 17:23:36 +0300

    vgg

--- src/config/db.php
+++ src/config/db.php
@@ -1,13 +1,20 @@
 <?php
-$host = 'db';
+// Railway'den aldığın güncel bilgiler
+$host = 'mysql.railway.internal';
+$db_name = 'railway';
 $username = 'root';
-$password = '123';
+$password = 'RDTRTiDskejQJKkKxnqsmvfKAjVnmHoy';
+$port = 3306; // Railway varsayılan MySQL portu
 
 try {
-    $db_name = 'kyk_yemek_db';
-    $db = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
-    //echo "Veritabanına başarıyla bağlanıldı!";
+    // Bağlantı dizesini güncelledik
+    $db = new PDO("mysql:host=$host;port=$port;dbname=$db_name;charset=utf8", $username, $password);
+    
+    // Hata raporlamayı açalım (Geliştirme aşamasında hayat kurtarır)
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // echo "Bağlantı Başarılı!"; 
 } catch (PDOException $exception) {
+    // Bağlantı hatası olursa burası çalışır
     echo "Veritabanı bağlantısı başarısız: " . $exception->getMessage();
 }
